90f651b875d40ba912ca9f1f90d9bd5ef45b9a4e
pedrobacchini <pedrobacchini@outlook.com>
2022-09-05 14:04:37 -0300

    create project

---
+++ src/main/java/com/github/pedrobacchini/batchkafka/BatchKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.github.pedrobacchini.batchkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pedrobacchini/batchkafka/BatchKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pedrobacchini.batchkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
