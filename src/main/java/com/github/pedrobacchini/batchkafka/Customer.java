f045a820a4f13144cec0849961655c710ee2d076
pedrobacchini <pedrobacchini@outlook.com>
2022-09-05 15:09:04 -0300

    producer kafka

---
+++ src/main/java/com/github/pedrobacchini/batchkafka/Customer.java
@@ -0,0 +1,13 @@
+package com.github.pedrobacchini.batchkafka;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/github/pedrobacchini/batchkafka/producer/ProducerApplication.java
@@ -0,0 +1,71 @@
+package com.github.pedrobacchini.batchkafka.producer;
+
+import com.github.pedrobacchini.batchkafka.Customer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.kafka.KafkaItemWriter;
+import org.springframework.batch.item.kafka.builder.KafkaItemWriterBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@EnableBatchProcessing
+@SpringBootApplication
+@RequiredArgsConstructor
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final KafkaTemplate<Long, Customer> kafkaTemplate;
+
+    @Bean
+    Job job() {
+        return jobBuilderFactory.get("job")
+            .start(start())
+            .incrementer(new RunIdIncrementer())
+            .build();
+    }
+
+    @Bean
+    Step start() {
+
+        var id = new AtomicLong();
+        final var customerItemReader = new ItemReader<Customer>() {
+            @Override
+            public Customer read() {
+                if (id.incrementAndGet() < 10_1000)
+                    return new Customer(id.incrementAndGet(), Math.random() > .5 ? "Jane" : "Jose");
+
+                return null;
+            }
+        };
+
+        return stepBuilderFactory.get("start")
+            .<Customer, Customer>chunk(10)
+            .reader(customerItemReader)
+            .writer(kafkaItemWriter())
+            .build();
+    }
+
+    @Bean
+    KafkaItemWriter<Long, Customer> kafkaItemWriter() {
+        return new KafkaItemWriterBuilder<Long, Customer>()
+            .kafkaTemplate(kafkaTemplate)
+            .itemKeyMapper(Customer::getId)
+            .build();
+    }
+
+}
